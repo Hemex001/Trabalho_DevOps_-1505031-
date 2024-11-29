682a1cd3f23a0a710ac058c6f698163a7e5af0dc
Hemex001 <83310574+Hemex001@users.noreply.github.com>
2024-11-29 13:36:21 -0300

    Alteração do aluno: apenas nome e RA

--- test_app.py
+++ test_app.py
@@ -1,40 +1,36 @@
 import unittest
-import json
 from app import app, db, Aluno
 
-class TestAlunoMariaDB(unittest.TestCase):
+class TestApp(unittest.TestCase):
     def setUp(self):
-        # Configurar o banco de dados para o contêiner MariaDB
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root_password@mariadb/school_db'
+        # Configuração inicial para o banco de dados em memória (para testes)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
         app.config['TESTING'] = True
         self.app = app.test_client()
-
-        # Criar as tabelas no banco
         with app.app_context():
             db.create_all()
 
     def tearDown(self):
-        # Limpar o banco de dados após cada teste
+        # Limpar o banco de dados após os testes
         with app.app_context():
             db.session.remove()
             db.drop_all()
 
-    def test_adicionar_aluno(self):
+    def test_cadastrar_aluno(self):
+        # Dados do aluno a serem cadastrados
         aluno_data = {
-            'nome': 'Teste',
-            'sobrenome': 'Teste 2',
-            'turma': '3B',
-            'disciplinas': 'Teste'
+            "nome": "João",
+            "ra": "1775235"
         }
-        response = self.app.post('/alunos', data=json.dumps(aluno_data), content_type='application/json')
+        # Fazendo o POST para cadastrar o aluno
+        response = self.app.post('/alunos', json=aluno_data)
         self.assertEqual(response.status_code, 201)
-        self.assertIn(b'Aluno adicionado com sucesso!', response.data)
 
-        # Verificar no banco de dados
+        # Verificando se o aluno foi salvo no banco de dados
         with app.app_context():
-            aluno = Aluno.query.filter_by(ra='654321').first()
+            aluno = Aluno.query.filter_by(ra="1775235").first()
             self.assertIsNotNone(aluno)
-            self.assertEqual(aluno.nome, 'Teste')
+            self.assertEqual(aluno.nome, "João")
 
 if __name__ == '__main__':
     unittest.main()
